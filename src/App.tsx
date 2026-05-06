e013d06b0949d37fdf51b34d141bd9117ff341e6
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2026-05-06 14:43:19 -0700

    deleting unnessary folder and file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "@/pages/HomePage";
 import { AboutPage } from "@/pages/AboutPage";
 import { BookConsultationPage } from "@/pages/BookConsultationPage";
